2e1be0127da4159ae1a17e44f35429b20686b710
Eashanthsai <eashanthsaineella@gmail.com>
2026-06-21 18:53:04 +0530

    Tenth code

---
+++ Simple_Pattern_Printing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Simple_Pattern_Printing {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of rows for the pattern : ");
+    int n = sc.nextInt();
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= i ; j++)
+        {
+            System.out.print("* ");
+        }
+    System.out.println();
+    }        
+
+    }
+}
